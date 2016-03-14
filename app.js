4840c8b9bf918644ed7fd1e0f9d9a3c48b81bef0
Leonard László <laslaul@yahoo.com>
2016-03-14 11:04:49 +0100

    Updated to Express v4.* and removed node_modules

--- app.js
+++ app.js
@@ -1,7 +1,3 @@
-/**
- * Module dependencies.
- */
-
 var express = require('express');
 var routes = require('./routes');
 var user = require('./routes/user');
@@ -9,33 +5,33 @@ var http = require('http');
 var path = require('path');
 var passport = require('passport');
 var LocalStrategy = require('passport-local').Strategy;
-var mongoose = require('mongoose/');
-
+var mongoose = require('mongoose');
+var bodyParser = require('body-parser');
+var logger = require('morgan');
+var methodOverride = require('method-override');
+var errorhandler = require('errorhandler');
 
 mongoose.connect('mongodb://localhost/MyDatabase');
 var app = express();
+var router = express.Router();
 
-// all environments
 app.set('port', process.env.PORT || 3001);
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
-app.use(express.favicon());
-app.use(express.logger('dev'));
-app.use(express.json());
-app.use(express.urlencoded());
-app.use(express.methodOverride());
+app.use(logger('dev'));
+app.use(methodOverride('X-HTTP-Method-Override'))
 app.use(passport.initialize());
 app.use(passport.session()); 
-app.use(app.router);
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
 
 // development only
 if ('development' == app.get('env')) {
-  app.use(express.errorHandler());
+  app.use(errorhandler());
 }
 
 var Schema = mongoose.Schema;
-
 
 var UserDetail = new Schema({
     username: String,
@@ -52,7 +48,6 @@ passport.deserializeUser(function(user, done) {
   done(null, user);
 });
 
-
 passport.use(new LocalStrategy(
   function(username, password, done) {
    
@@ -68,28 +63,30 @@ passport.use(new LocalStrategy(
   }
 ));
 
-
-
-app.get('/auth', function(req, res, next) {
-  res.sendfile('views/login.html');
+router.get('/auth', function(req, res, next) {
+  res.sendFile('views/login.html', { root: __dirname });
 });
 
-
-app.get('/loginFailure' , function(req, res, next){
+router.get('/loginFailure' , function(req, res, next){
 	res.send('Failure to authenticate');
 });
 
-app.get('/loginSuccess' , function(req, res, next){
+router.get('/loginSuccess' , function(req, res, next){
 	res.send('Successfully authenticated');
 });
 
-app.post('/login',
+router.get('/login', function(req, res) {
+  res.sendFile('views/login.html', { root: __dirname });
+});
+
+router.post('/login',
   passport.authenticate('local', {
     successRedirect: '/loginSuccess',
     failureRedirect: '/loginFailure'
   }));
 
+app.use('/', router);
+
 http.createServer(app).listen(app.get('port'), function(){
   console.log('Express server listening on port ' + app.get('port'));
 });
-
